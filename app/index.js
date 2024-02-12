7192dc48268394a364aab366ec5494688dd032b6
jt151077 <jt151077@gmail.com>
2024-02-12 08:47:52 +0000

    Added binary authorization to clusters

--- app/index.js
+++ app/index.js
@@ -5,8 +5,7 @@ const port = 80;
 async function main() {
   const server = http.createServer(async function (req, res) {
     if (req.url == '/') {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.write(JSON.stringify({"message": "Signicat Demo"}));
+        res.write(JSON.stringify({message: 'Signicat Demo'}));
         res.end()
     }
   }).listen(port);
