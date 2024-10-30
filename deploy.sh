b54a84c2262887cc6546c63b85f591eb3b264d95
jt151077 <jt151077@gmail.com>
2024-10-30 14:07:56 +0000

    trying to create namespace via skaffold

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,9 @@ echo 'Building and deploying the latest Docker image'
 project=$(grep -o '\"project_id\": \"[^\"]*' terraform.tfvars.json | grep -o '[^\"]*$')
 region=$(grep -o '\"project_default_region\": \"[^\"]*' terraform.tfvars.json | grep -o '[^\"]*$')
 
+
+gcloud container clusters get-credentials dev-cluster --location=$region --project=$project
+kubectl create namespace jeremy-ns-dev
+gcloud container clusters get-credentials prod-cluster --location=$region --project=$project
+kubectl create namespace jeremy-ns-prod
 gcloud builds submit --config=cloudbuild.yaml --project=$project --substitutions=_REGION=$region --region=$region 
